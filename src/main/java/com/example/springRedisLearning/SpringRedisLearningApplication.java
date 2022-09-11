0551b4db5f013c97d754fa8b528bef7709b5d441
natdanaimod <tobmnatdanai@gmail.com>
2022-09-11 09:26:09 +0700

    first commit

---
+++ src/main/java/com/example/springRedisLearning/SpringRedisLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.springRedisLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springRedisLearning/SpringRedisLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springRedisLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
